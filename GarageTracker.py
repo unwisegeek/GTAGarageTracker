25202df03cabd1a61abb21255d9d5e4318c780d6
John Madon <john@unwisegeek.net>
2019-08-06 03:12:40 -0400

    * Added vehicle and garage lists with sorting

--- GarageTracker.py
+++ GarageTracker.py
@@ -1,10 +1,74 @@
 import PyQt5
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from Main_Window import Ui_MainWindow
 import pandas as pd, sys
 
-# Load Data into Pandas Dataframe
-vehicle_data = pd.read_csv('vehicles.csv')
+global_vehicle_data = pd.read_csv("vehicles.csv",sep=";",engine='python')
+global_garage_data = pd.read_csv("garages.csv",sep=";",engine='python')
+
+class DataFrameModel(QtCore.QAbstractTableModel):
+    DtypeRole = QtCore.Qt.UserRole + 1000
+    ValueRole = QtCore.Qt.UserRole + 1001
+
+    def __init__(self, df=pd.DataFrame(), parent=None):
+        super(DataFrameModel, self).__init__(parent)
+        self._dataframe = df
+
+    def setDataFrame(self, dataframe):
+        self.beginResetModel()
+        self._dataframe = dataframe.copy()
+        self.endResetModel()
+
+    def dataFrame(self):
+        return self._dataframe
+
+    dataFrame = QtCore.pyqtProperty(pd.DataFrame, fget=dataFrame, fset=setDataFrame)
+
+    @QtCore.pyqtSlot(int, QtCore.Qt.Orientation, result=str)
+    def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role: int = QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return self._dataframe.columns[section]
+            else:
+                return str(self._dataframe.index[section])
+        return QtCore.QVariant()
+
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        if parent.isValid():
+            return 0
+        return len(self._dataframe.index)
+
+    def columnCount(self, parent=QtCore.QModelIndex()):
+        if parent.isValid():
+            return 0
+        return self._dataframe.columns.size
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if not index.isValid() or not (0 <= index.row() < self.rowCount()
+            and 0 <= index.column() < self.columnCount()):
+            return QtCore.QVariant()
+        row = self._dataframe.index[index.row()]
+        col = self._dataframe.columns[index.column()]
+        dt = self._dataframe[col].dtype
+
+        val = self._dataframe.iloc[row][col]
+        if role == QtCore.Qt.DisplayRole:
+            return str(val)
+        elif role == DataFrameModel.ValueRole:
+            return val
+        if role == DataFrameModel.DtypeRole:
+            return dt
+        return QtCore.QVariant()
+
+    def roleNames(self):
+        roles = {
+            QtCore.Qt.DisplayRole: b'display',
+            DataFrameModel.DtypeRole: b'dtype',
+            DataFrameModel.ValueRole: b'value'
+        }
+        return roles
 
 class MainWindow():
     def __init__(self):
@@ -12,41 +76,64 @@ class MainWindow():
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.main_win)
 
+        # Load Vehicle Data
+        local_vehicle_data = pd.read_csv("vehicles.csv",sep=";",engine='python')
+
+        # Set Vehicle SortBy Options
+        local_vehicle_data_headers = []
+        for col in global_vehicle_data.columns:
+            local_vehicle_data_headers += [col]
+
         # Configure Vehicle Table
-        self.ui.tableVehicle.setRowCount(vehicle_data['Vehicle'].count())
-        self.ui.tableVehicle.setColumnCount(4)
-        self.ui.tableVehicle.setHorizontalHeaderLabels(["Name","Class","Owned","Garage"])
+        vmodel = DataFrameModel(local_vehicle_data)
+        self.ui.tableVehicle.setModel(vmodel)
+        self.ui.tableVehicle.resizeColumnsToContents()
 
-        local_vehicle_data = vehicle_data
+        # Load Garage Data
+        local_garage_data = pd.read_csv("garages.csv", sep=";", engine='python')
 
-        # Populate SortBy Combo Box
-        self.ui.comboVehiclesSortBy.clear()
-        local_vehicle_data_headers = list(local_vehicle_data.columns.values)
+        # Load Garage SortBy Options
+        local_garage_data_headers = []
+        for col in local_garage_data.columns:
+            local_garage_data_headers += [col]
+
+        # Configure Garage Table
+        gmodel = DataFrameModel(local_garage_data)
+        self.ui.tableGarage.setModel(gmodel)
+        self.ui.tableGarage.resizeColumnsToContents()
+
+        # Populate Vehicle SortBy Combo Box
         for idx in range(0, len(local_vehicle_data_headers)):
             self.ui.comboVehiclesSortBy.addItem(local_vehicle_data_headers[idx])
 
-        # Sort Table Based On comboVehiclesSortBy
-        self.refresh()
+        # Populate Garages SortBy Combo Box
+        for idx in range(0, len(local_garage_data_headers)):
+            self.ui.comboGaragesSortBy.addItem(local_garage_data_headers[idx])
 
-        # Populate Table
-        for idx in range(0, vehicle_data['Vehicle'].count()):
-            self.ui.tableVehicle.setItem(idx, 0, PyQt5.QtWidgets.QTableWidgetItem(local_vehicle_data['Vehicle'][idx]))
-            self.ui.tableVehicle.setItem(idx, 1, PyQt5.QtWidgets.QTableWidgetItem(local_vehicle_data['Class'][idx]))
-            self.ui.tableVehicle.setItem(idx, 2, PyQt5.QtWidgets.QTableWidgetItem(str(local_vehicle_data['Owned'][idx])))
-            self.ui.tableVehicle.setItem(idx, 3, PyQt5.QtWidgets.QTableWidgetItem(str(local_vehicle_data['Garage'][idx])))
+        # Set Vehicles as Active Tab
+        self.ui.tabWidget.setCurrentWidget(self.ui.tabVehicles)
 
-        self.qTimer = QTimer()
-        self.qTimer.setInterval(500)
-        self.qTimer.timeout.connect(lambda: self.refresh())
-        self.qTimer.start()
+        # Connections to Slots
+        self.ui.comboVehiclesSortBy.activated.connect(self.vehicle_sort_criteria_changed)
+        self.ui.comboGaragesSortBy.activated.connect(self.garage_sort_criteria_changed)
 
-    def refresh(self):
+    def vehicle_sort_criteria_changed(self):
         sort_criteria = self.ui.comboVehiclesSortBy.currentText()
-        local_vehicle_data = local_vehicle_data.sort_values(sort_criteria)
+        vehicle_data = pd.read_csv("vehicles.csv",sep=";",engine='python')
+        new_vehicle_data = vehicle_data.sort_values(sort_criteria)
+        new_vehicle_data.to_csv("vehicles.csv", index=None, header=True,sep=";")
 
-    def showclick(self, item):
-        cellContent = item.data()
-        print("{} x {} was clicked!".format(item.column(), item.row()))
+        vmodel = DataFrameModel(pd.read_csv("vehicles.csv",sep=";",engine='python'))
+        self.ui.tableVehicle.setModel(vmodel)
+
+    def garage_sort_criteria_changed(self):
+        sort_criteria = self.ui.comboGaragesSortBy.currentText()
+        garage_data = pd.read_csv("garages.csv",sep=";",engine='python')
+        new_garage_data = garage_data.sort_values(sort_criteria)
+        new_garage_data.to_csv("garages.csv", index=None, header=True,sep=";")
+
+        gmodel = DataFrameModel(pd.read_csv("garages.csv",sep=";",engine='python'))
+        self.ui.tableGarage.setModel(gmodel)
 
     def show(self):
         self.main_win.show()
